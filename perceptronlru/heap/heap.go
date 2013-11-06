b7fabc0e576fc7bb24ce5bd3f149205f63212606
Alexandre Passos <alexandre.tp@gmail.com>
2013-11-06 08:57:32 -0500

    refactoring

---
+++ perceptronlru/heap/heap.go
@@ -0,0 +1,98 @@
+package heap
+
+type HeapItem struct {
+	priority float64
+	Position int
+	Value    interface{}
+}
+
+type ItemArray []*HeapItem
+
+type Heap struct {
+	Size     int
+	elements ItemArray
+}
+
+func NewHeap() *Heap {
+	return &Heap{
+		Size:     0,
+		elements: ItemArray{},
+	}
+}
+
+func (heap *Heap) Swap(i, j int) {
+	a := heap.elements[i]
+	heap.elements[i] = heap.elements[j]
+	heap.elements[j] = a
+	heap.elements[i].Position = i
+	heap.elements[j].Position = j
+}
+
+func (heap *Heap) Up(index int) {
+	for {
+		i := (index - 1) / 2 // parent
+		if i == index || !(heap.elements[index].priority < heap.elements[i].priority) {
+			break
+		}
+		heap.Swap(i, index)
+		index = i
+	}
+}
+
+func (heap *Heap) Down(index int) {
+	for {
+		j1 := 2*index + 1
+		if j1 >= heap.Size || j1 < 0 {
+			break
+		}
+		j := j1
+		p1 := heap.elements[j1].priority
+		j2 := j1 + 1
+		p2 := heap.elements[j2].priority
+		if j2 < heap.Size && !(p1 < p2) {
+			j = j2
+		}
+		if !(heap.elements[j].priority < heap.elements[index].priority) {
+			break
+		}
+		heap.Swap(index, j)
+		index = j
+	}
+}
+
+func (heap *Heap) Push(element *HeapItem) {
+	element.Position = heap.Size
+	heap.Size += 1
+	heap.elements = append(heap.elements, element)
+	heap.Up(element.Position)
+}
+
+func (heap *Heap) Insert(element interface{}, priority float64) *HeapItem {
+	item := &HeapItem{
+		Value:    element,
+		priority: priority,
+		Position: -1,
+	}
+	heap.Push(item)
+	return item
+}
+
+func (heap *Heap) Remove(index int) *HeapItem {
+	n := heap.Size - 1
+	heap.Swap(index, n)
+	heap.Down(index)
+	e := heap.elements[n]
+	heap.Size -= 1
+	heap.elements = heap.elements[0:heap.Size]
+	return e
+}
+
+func (heap *Heap) Reinsert(index int, priority float64) {
+	item := heap.Remove(index)
+	item.priority = priority
+	heap.Push(item)
+}
+
+func (heap *Heap) Pop() *HeapItem {
+	return heap.Remove(0)
+}
--- perceptronlru/perceptronlru.go
+++ perceptronlru/perceptronlru.go
@@ -18,102 +18,8 @@ limitations under the License.
 // next access time with a perceptron.
 package perceptronlru
 
-type HeapItem struct {
-	priority float64
-	position int
-	Value    interface{}
-}
+import 	"github.com/golang/groupcache/perceptronlru/heap"
 
-type ItemArray []*HeapItem
-
-type Heap struct {
-	size     int
-	elements ItemArray
-}
-
-func NewHeap() *Heap {
-	return &Heap{
-		size:     0,
-		elements: ItemArray{},
-	}
-}
-
-func (heap *Heap) Swap(i, j int) {
-	a := heap.elements[i]
-	heap.elements[i] = heap.elements[j]
-	heap.elements[j] = a
-	heap.elements[i].position = i
-	heap.elements[j].position = j
-}
-
-func (heap *Heap) Up(index int) {
-	for {
-		i := (index - 1) / 2 // parent
-		if i == index || !(heap.elements[index].priority < heap.elements[i].priority) {
-			break
-		}
-		heap.Swap(i, index)
-		index = i
-	}
-}
-
-func (heap *Heap) Down(index int) {
-	for {
-		j1 := 2*index + 1
-		if j1 >= heap.size || j1 < 0 {
-			break
-		}
-		j := j1
-		p1 := heap.elements[j1].priority
-		j2 := j1 + 1
-		p2 := heap.elements[j2].priority
-		if j2 < heap.size && !(p1 < p2) {
-			j = j2
-		}
-		if !(heap.elements[j].priority < heap.elements[index].priority) {
-			break
-		}
-		heap.Swap(index, j)
-		index = j
-	}
-}
-
-func (heap *Heap) Push(element *HeapItem) {
-	element.position = heap.size
-	heap.size += 1
-	heap.elements = append(heap.elements, element)
-	heap.Up(element.position)
-}
-
-func (heap *Heap) Insert(element interface{}, priority float64) *HeapItem {
-	item := &HeapItem{
-		Value:    element,
-		priority: priority,
-		position: -1,
-	}
-	heap.Push(item)
-	return item
-}
-
-func (heap *Heap) Remove(index int) *HeapItem {
-	n := heap.size - 1
-	heap.Swap(index, n)
-	heap.Down(index)
-	e := heap.elements[n]
-	heap.size -= 1
-	heap.elements = heap.elements[0:heap.size]
-	return e
-}
-
-func (heap *Heap) Reinsert(index int, priority float64) {
-	item := heap.Remove(index)
-	item.priority = priority
-	heap.Push(item)
-}
-
-func (heap *Heap) Pop() *HeapItem {
-	return heap.Remove(0)
-}
 
 // Cache is an LRU cache. It is not safe for concurrent access.
 type Cache struct {
@@ -122,17 +28,17 @@ type Cache struct {
 	OnEvicted func(key Key, value interface{})
 
 	operations int
-	heap       *Heap
-	cache      map[interface{}]*HeapItem
-	lastUse    map[interface{}]int
+	heap       *heap.Heap
+	cache      map[interface{}]*heap.HeapItem
 }
 
 // A Key may be any value that is comparable. See http://golang.org/ref/spec#Comparison_operators
 type Key interface{}
 
 type entry struct {
-	key   Key
-	value interface{}
+	key     Key
+	lastUse int
+	value   interface{}
 }
 
 // New creates a new Cache.
@@ -140,29 +46,28 @@ type entry struct {
 // that eviction is done by the caller.
 func New() *Cache {
 	return &Cache{
-		heap:       NewHeap(),
+		heap:       heap.NewHeap(),
 		operations: 0,
-		cache:      make(map[interface{}]*HeapItem),
-		lastUse:    make(map[interface{}]int),
+		cache:      make(map[interface{}]*heap.HeapItem),
 	}
 }
 
 // Add adds a value to the cache.
 func (c *Cache) Add(key Key, value interface{}) {
 	if c.cache == nil {
-		c.cache = make(map[interface{}]*HeapItem)
-		c.lastUse = make(map[interface{}]int)
-		c.heap = NewHeap()
+		c.cache = make(map[interface{}]*heap.HeapItem)
+		c.heap = heap.NewHeap()
 		c.operations = 0
 	}
 	if ee, ok := c.cache[key]; ok {
 		c.operations += 1
-		c.heap.Reinsert(ee.position, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
+		c.heap.Reinsert(ee.Position, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
+		ee.Value.(*entry).lastUse = c.operations
 		ee.Value.(*entry).value = value
 		return
 	}
 	c.operations += 1
-	ele := c.heap.Insert(&entry{key, value}, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
+	ele := c.heap.Insert(&entry{key, c.operations, value}, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
 	c.cache[key] = ele
 }
 
@@ -173,7 +78,8 @@ func (c *Cache) Get(key Key) (value interface{}, ok bool) {
 	}
 	c.operations += 1
 	if ele, hit := c.cache[key]; hit {
-		c.heap.Reinsert(ele.position, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
+		c.heap.Reinsert(ele.Position, float64(-c.operations)) // TODO(apassos): perceptron decision goes here
+		ele.Value.(*entry).lastUse = c.operations
 		return ele.Value.(*entry).value, true
 	}
 	return
@@ -201,9 +107,9 @@ func (c *Cache) RemoveOldest() {
 	}
 }
 
-func (c *Cache) removeElement(e *HeapItem) {
+func (c *Cache) removeElement(e *heap.HeapItem) {
 	// TODO(apassos): perceptron update goes here
-	c.heap.Remove(e.position)
+	c.heap.Remove(e.Position)
 	kv := e.Value.(*entry)
 	delete(c.cache, kv.key)
 	if c.OnEvicted != nil {
@@ -216,5 +122,5 @@ func (c *Cache) Len() int {
 	if c.cache == nil {
 		return 0
 	}
-	return c.heap.size
+	return c.heap.Size
 }
